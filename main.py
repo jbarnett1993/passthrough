1c3e405bef1ab36f91a623de48791d328b098815
jbarnett1993 <jamesbarnett1993@icloud.com>
2023-10-21 16:46:36 +0100

    new code

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ df = df[["CPSESSIONID","BANKINST","SESSIONTYPE","TRADEDATE","ORDERTYPE","IS_WINN
 df = df[df.SESSIONTYPE=="RFS"]
 df = df[df.ORDERTYPE != "SWAP"]
 
-sessions = df["CPSESSIONID"].unique()
 
 df["DIRECTION"].replace("BUY","OFFER",inplace=True)
 
@@ -42,8 +41,6 @@ for x,row in df.iterrows():
 
 pivot_df = df.pivot_table(index=['CPSESSIONID',"TRADEDATE",'BANKINST','QUOTEPAIR',
                                  'GIVENCCY',"FIRSTCCY",'GIVENAMT','SIZE_GROUP'], columns='DIRECTION',values='SPOTRATE')
-# pivot_df["MIDPOINT"] = 
-# df.loc[mask, "GIVENAMT"] = df["GIVENAMT"] / df["MIDPOINT"]
 
 
 
@@ -51,12 +48,12 @@ pivot_df.columns.name = None
 
 mask1 = np.logical_and(pivot_df["BID"]==0 , pivot_df["OFFER"]==0)
 pivot_df.loc[mask1,['BID','OFFER']]=np.nan
-
+print(pivot_df)
 
 pivot_df = pivot_df.reset_index()
 
 pivot_df.reset_index(drop=True,inplace=True)
-
+print(pivot_df)
 
 pivot_df['SPREAD'] = np.where(pivot_df['QUOTEPAIR'].str.contains('JPY'),(pivot_df['OFFER'] - pivot_df['BID']) * 100, (pivot_df['OFFER'] - pivot_df['BID']) * 10000)
 
@@ -71,14 +68,16 @@ def assign_size_group(givenamt):
     if givenamt < 10000000:
         return "0-10"
     elif givenamt < 20000000:
-        return "20-30"
+        return "10-20"
     elif givenamt < 30000000:
-        return "30-40"
+        return "20-30"
     elif givenamt < 40000000:
-        return "40-50"
+        return "30-40"
     elif givenamt < 50000000:
-        return "50-75"
+        return "40-50"
     elif givenamt < 75000000:
+        return "50-75"
+    elif givenamt < 100000000:
         return "75-100"
     else:
         return "100+"
@@ -89,20 +88,27 @@ mask_jpy = pivot_df.loc[mask, 'QUOTEPAIR'].str.contains('JPY')
 pivot_df.loc[mask & mask_jpy, 'SPREAD'] = -(pivot_df.loc[mask & mask_jpy, 'OFFER'] - pivot_df.loc[mask & mask_jpy, 'BID']) * 100
 pivot_df.loc[mask & ~mask_jpy, 'SPREAD'] = -(pivot_df.loc[mask & ~mask_jpy, 'OFFER'] - pivot_df.loc[mask & ~mask_jpy, 'BID']) * 10000
 
+
+
+
+
 pivot_df.reset_index(drop=True, inplace=True)
 
 
 pivot_df['TRADEDATE'] = pd.to_datetime(pivot_df["TRADEDATE"])
-average_spread = pivot_df.groupby(['CPSESSIONID','TRADEDATE','QUOTEPAIR','SIZE_GROUP'])['SPREAD'].mean()
-average_spread = average_spread.reset_index()
+pivot_df.to_csv("masterdata.csv")
 
-average_spread.to_csv("cleaned_data.csv", index=False)
-import matplotlib.pyplot as plt 
-
-quote_pairs = average_spread["QUOTEPAIR"].unique()
+average_spread_per_ccy_and_size = pivot_df.groupby(['CPSESSIONID','TRADEDATE','QUOTEPAIR','SIZE_GROUP'])['SPREAD'].mean()
 
 
+average_spread_per_ccy_and_size = average_spread_per_ccy_and_size.reset_index()
 
+
+average_spread_per_ccy_and_size = pivot_df.groupby(['CPSESSIONID','TRADEDATE','QUOTEPAIR','SIZE_GROUP'])['SPREAD'].mean()
+
+
+average_spread_per_bank_ccy_and_size = pivot_df.groupby(['QUOTEPAIR','SIZE_GROUP','BANKINST'])['SPREAD'].mean()
+print(average_spread_per_bank_ccy_and_size)
 
 
 # for quote_pair in quote_pairs:
