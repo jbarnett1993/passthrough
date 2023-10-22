ad5d0de66093947d73555e94e587b62877c35631
jbarnett1993 <64900909+jbarnett1993@users.noreply.github.com>
2023-10-22 17:53:23 +0100

    Update cs50.py

--- cs50.py
+++ cs50.py
@@ -13,3 +13,30 @@ In a file called adieu.py, implement a program that prompts the user for names, 
  commas and one and, as in the below:
 
 use the inflect module
+
+import inflect
+import sys
+
+def get_names():
+    """Prompt the user for names until they input Ctrl-D."""
+    names = []
+    while True:
+        try:
+            name = input("Enter a name (Ctrl-D to stop): ")
+            names.append(name)
+        except EOFError:
+            break
+    return names
+
+def main():
+    p = inflect.engine()
+    names = get_names()
+
+    if not names:
+        sys.exit("No names were provided.")
+
+    farewell_message = f"Adieu, adieu, to {p.join(names)}"
+    print(farewell_message)
+
+if __name__ == "__main__":
+    main()
