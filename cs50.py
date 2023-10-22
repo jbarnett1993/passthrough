6b7cff22601bec6fb1cff57ad461172099613c51
jbarnett1993 <jamesbarnett1993@icloud.com>
2023-10-22 18:01:46 +0100

    next q

--- cs50.py
+++ cs50.py
@@ -1,28 +1,8 @@
-import inflect
-import sys
-
-def get_names():
-    """Prompt the user for names until they input Ctrl-D or Ctrl-Z."""
-    names = []
-    while True:
-        try:
-            name = input("Enter a name (Ctrl-D or Ctrl-Z to stop, or just press Enter): ")
-            if not name:  # exit loop if name is empty
-                break
-            names.append(name)
-        except EOFError:
-            break
-    return names
-
-def main():
-    p = inflect.engine()
-    names = get_names()
-
-    if not names:
-        sys.exit("No names were provided.")
-
-    farewell_message = f"Adieu, adieu, to {p.join(names)}"
-    print(farewell_message)
-
-if __name__ == "__main__":
-    main()
+'''Prompts the user for a level, 
+. If the user does not input a positive integer, the program should prompt again.
+Randomly generates an integer between 1 and 
+, inclusive, using the random module.
+Prompts the user to guess that integer. If the guess is not a positive integer, the program should prompt the user again.
+If the guess is smaller than that integer, the program should output Too small! and prompt the user again.
+If the guess is larger than that integer, the program should output Too large! and prompt the user again.
+If the guess is the same as that integer, the program should output Just right! and exit.'''
