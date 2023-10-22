2c46c4a060fdf6558915b90e0658b8afeb7495c4
jbarnett1993 <64900909+jbarnett1993@users.noreply.github.com>
2023-10-22 17:55:45 +0100

    Update cs50.py

--- cs50.py
+++ cs50.py
@@ -1,28 +1,14 @@
-In The Sound of Music, there’s a song sung largely in English, So Long, Farewell, with these lyrics, wherein “adieu” means “goodbye” in French:
-
-Adieu, adieu, to yieu and yieu and yieu
-
-Of course, the line isn’t grammatically correct, since it would typically be written (with an Oxford comma) as:
-
-Adieu, adieu, to yieu, yieu, and yieu
-
-To be fair, “yieu” isn’t even a word; it just rhymes with “you”!
-
-In a file called adieu.py, implement a program that prompts the user for names, one per line, until the user inputs control-d. Assume that the user will input at least one name. Then bid adieu to those names, separating two names with one and, three names with two commas and one and, and 
- names with 
- commas and one and, as in the below:
-
-use the inflect module
-
 import inflect
 import sys
 
 def get_names():
-    """Prompt the user for names until they input Ctrl-D."""
+    """Prompt the user for names until they input Ctrl-D or Ctrl-Z."""
     names = []
     while True:
         try:
-            name = input("Enter a name (Ctrl-D to stop): ")
+            name = input("Enter a name (Ctrl-D or Ctrl-Z to stop, or just press Enter): ")
+            if not name:  # exit loop if name is empty
+                break
             names.append(name)
         except EOFError:
             break
