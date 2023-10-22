6e094e5bf8b7f52e686d26374a06e6364389895f
jbarnett1993 <64900909+jbarnett1993@users.noreply.github.com>
2023-10-22 18:04:03 +0100

    Update cs50.py

--- cs50.py
+++ cs50.py
@@ -6,3 +6,40 @@ Prompts the user to guess that integer. If the guess is not a positive integer, 
 If the guess is smaller than that integer, the program should output Too small! and prompt the user again.
 If the guess is larger than that integer, the program should output Too large! and prompt the user again.
 If the guess is the same as that integer, the program should output Just right! and exit.'''
+
+import random
+
+def get_positive_integer(prompt):
+    """Prompts the user for a positive integer and returns it."""
+    while True:
+        try:
+            num = int(input(prompt))
+            if num > 0:
+                return num
+            else:
+                print("Please enter a positive integer.")
+        except ValueError:
+            print("Please enter a valid positive integer.")
+
+def main():
+    # Prompt the user for a level
+    level = get_positive_integer("Enter a level (positive integer): ")
+
+    # Randomly generate an integer between 1 and level, inclusive
+    secret_number = random.randint(1, level)
+
+    while True:
+        # Prompt the user to guess the integer
+        guess = get_positive_integer("Guess the integer between 1 and {}: ".format(level))
+
+        # Check the guess
+        if guess < secret_number:
+            print("Too small!")
+        elif guess > secret_number:
+            print("Too large!")
+        else:
+            print("Just right!")
+            break
+
+if __name__ == "__main__":
+    main()
