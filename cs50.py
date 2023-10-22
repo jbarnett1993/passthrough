1e186f8987451f25954645764a4b9c66d3431345
jbarnett1993 <jamesbarnett1993@icloud.com>
2023-10-22 18:38:31 +0100

    next question

--- cs50.py
+++ cs50.py
@@ -1,68 +1,54 @@
 '''
+In a file called bitcoin.py, implement a program that:
 
-One of David’s first toys as a child, funny enough, was Little Professor, a “calculator” that would generate ten different math problems for David to solve. For instance, if the toy were to display 4 + 0 = , David would (hopefully) answer with 4. If the toy were to display 4 + 1 = , David would (hopefully) answer with 5. If David were to answer incorrectly, the toy would display EEE. And after three incorrect answers for the same problem, the toy would simply display the correct answer (e.g., 4 + 0 = 4 or 4 + 1 = 5).
+Expects the user to specify as a command-line argument the number of Bitcoins, 
+, that they would like to buy. If that argument cannot be converted to a float, the program should exit via sys.exit with an error message.
+Queries the API for the CoinDesk Bitcoin Price Index at https://api.coindesk.com/v1/bpi/currentprice.json, which returns a JSON object, among whose nested keys is the current price of Bitcoin as a float. Be sure to catch any exceptions, as with code like:
+import requests
 
-In a file called professor.py, implement a program that:
+try:
+    ...
+except requests.RequestException:
+    ...
+Outputs the current cost of 
+ Bitcoins in USD to four decimal places, using , as a thousands separator.
+Hints
+Recall that the sys module comes with argv, per docs.python.org/3/library/sys.html#sys.argv.
+Note that the requests module comes with quite a few methods, per requests.readthedocs.io/en/latest, among which are get, per requests.readthedocs.io/en/latest/user/quickstart/#make-a-request, and json, per requests.readthedocs.io/en/latest/user/quickstart/#json-response-content. You can install it with:
+pip install requests
+Note that CoinDesk’s API returns a JSON response like:
+{
+   "time":{
+      "updated":"May 2, 2022 15:27:00 UTC",
+      "updatedISO":"2022-05-02T15:27:00+00:00",
+      "updateduk":"May 2, 2022 at 16:27 BST"
+   },
+   "disclaimer":"This data was produced from the CoinDesk Bitcoin Price Index (USD). Non-USD currency data converted using hourly conversion rate from openexchangerates.org",
+   "chartName":"Bitcoin",
+   "bpi":{
+      "USD":{
+         "code":"USD",
+         "symbol":"&#36;",
+         "rate":"38,761.0833",
+         "description":"United States Dollar",
+         "rate_float":38761.0833
+      },
+      "GBP":{
+         "code":"GBP",
+         "symbol":"&pound;",
+         "rate":"30,827.6198",
+         "description":"British Pound Sterling",
+         "rate_float":30827.6198
+      },
+      "EUR":{
+         "code":"EUR",
+         "symbol":"&euro;",
+         "rate":"36,800.2764",
+         "description":"Euro",
+         "rate_float":36800.2764
+      }
+   }
+}
 
-Prompts the user for a level, 
-. If the user does not input 1, 2, or 3, the program should prompt again.
-Randomly generates ten (10) math problems formatted as X + Y = , wherein each of X and Y is a non-negative integer with 
- digits. No need to support operations other than addition (+).
-Prompts the user to solve each of those problems. If an answer is not correct (or not even a number), the program should output EEE and prompt the user again, allowing the user up to three tries in total for that problem. If the user has still not answered correctly after three tries, the program should output the correct answer.
-The program should ultimately output the user’s score: the number of correct answers out of 10.
-Structure your program as follows, wherein get_level prompts (and, if need be, re-prompts) the user for a level and returns 1, 2, or 3, and generate_integer returns a randomly generated non-negative integer with level digits or raises a ValueError if level is not 1, 2, or 3:
 
 '''
-
-import random
-
-def get_level():
-    while True:
-        level = input("Enter a level (1, 2, or 3): ")
-        if level in ['1', '2', '3']:
-            return int(level)
-        else:
-            print("Invalid input. Please enter 1, 2, or 3.")
-
-def generate_integer(level):
-    if level == 1:
-        return random.randint(0, 9)
-    elif level == 2:
-        return random.randint(10, 99)
-    elif level == 3:
-        return random.randint(100, 999)
-    else:
-        raise ValueError("Invalid level")
-
-def main():
-    level = get_level()
-    
-    correct_count = 0
-    
-    for _ in range(10):
-        x = generate_integer(level)
-        y = generate_integer(level)
-        correct_answer = x + y
-
-        tries = 0
-        while tries < 3:
-            answer = input(f"{x} + {y} = ")
-
-            try:
-                if int(answer) == correct_answer:
-                    correct_count += 1
-                    break
-                else:
-                    print("EEE")
-                    tries += 1
-            except ValueError:
-                print("EEE")
-                tries += 1
-
-        if tries == 3:
-            print(f"The correct answer is: {correct_answer}")
-
-    print(f"Your score is: {correct_count}/10")
-
-if __name__ == "__main__":
-    main()
