346eadd703fea9932d8515cb29b1dae6f3b32514
jbarnett1993 <jamesbarnett1993@icloud.com>
2023-10-22 17:57:04 +0100

    help

--- cs50.py
+++ cs50.py
@@ -1,19 +1,3 @@
-In The Sound of Music, there’s a song sung largely in English, So Long, Farewell, with these lyrics, wherein “adieu” means “goodbye” in French:
-
-Adieu, adieu, to yieu and yieu and yieu
-
-Of course, the line isn’t grammatically correct, since it would typically be written (with an Oxford comma) as:
-
-Adieu, adieu, to yieu, yieu, and yieu
-
-To be fair, “yieu” isn’t even a word; it just rhymes with “you”!
-
-In a file called adieu.py, implement a program that prompts the user for names, one per line, until the user inputs control-d. Assume that the user will input at least one name. Then bid adieu to those names, separating two names with one and, three names with two commas and one and, and 
- names with 
- commas and one and, as in the below:
-
-use the inflect module
-
 import inflect
 import sys
 
