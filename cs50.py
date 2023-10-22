3edbc8e3eda69b3ab124e4159504885683c86b77
jbarnett1993 <jamesbarnett1993@icloud.com>
2023-10-22 16:53:41 +0100

    cs50 attempt 2

---
+++ cs50.py
@@ -0,0 +1,3 @@
+ '''implement a program that prompts the user for a str in English and then outputs the “emojized” version of that str, converting any codes (or aliases) therein to their corresponding emoji.
+
+ carpedm20.github.io/emoji/all.html?enableList=enable_list_alias '''
