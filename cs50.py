48e7c332da2b31335307df8e65c4dbeb2f663d13
jbarnett1993 <jamesbarnett1993@icloud.com>
2023-10-22 17:29:01 +0100

    code not working

--- cs50.py
+++ cs50.py
@@ -1,19 +1,28 @@
-FIGlet, named after Frank, Ian, and Glen’s letters, is a program from the early 1990s for making large letters out of ordinary text, a form of ASCII art:
+import sys
+import random
+import pyfiglet
 
- _ _ _          _   _     _
-| (_) | _____  | |_| |__ (_)___
-| | | |/ / _ \ | __| '_ \| / __|
-| | |   <  __/ | |_| | | | \__ \
-|_|_|_|\_\___|  \__|_| |_|_|___/
-Among the fonts supported by FIGlet are those at figlet.org/examples.html.
 
-FIGlet has since been ported to Python as a module called pyfiglet.
 
-In a file called figlet.py, implement a program that:
+def main():
+    if len(sys.argv) not in [0,2]:
+        sys.exit("incorrect usage")
 
-Expects zero or two command-line arguments:
-Zero if the user would like to output text in a random font.
-Two if the user would like to output text in a specific font, in which case the first of the two should be -f or --font, and the second of the two should be the name of the font.
-Prompts the user for a str of text.
-Outputs that text in the desired font.
-If the user provides two command-line arguments and the first is not -f or --font or the second is not the name of a font, the program should exit via sys.exit with an error message.
+    if len(sys.argv) == 2:
+        if sys.argv[0] not in ['-f','--font']:
+            sys.exit("first input not correct")
+
+        font = sys.argv[2]
+        if font not in pyfiglet.FigletFont.getFonts():
+            sys.exit(f"Font {font} is not supported")
+
+        else:
+            font = random.choice(pyfiglet.FigletFont.getFonts())
+
+        text = input("Enter text:")
+
+        result = pyfiglet.figlet_format(text, font=font)
+        print(result)
+
+    if __name__ == "__main__":
+        main()
