b415f98b05c7a5ecc6440dd24c0c27aa52f052db
jbarnett1993 <64900909+jbarnett1993@users.noreply.github.com>
2023-10-22 18:39:52 +0100

    Update cs50.py

--- cs50.py
+++ cs50.py
@@ -1,54 +1,39 @@
-'''
-In a file called bitcoin.py, implement a program that:
-
-Expects the user to specify as a command-line argument the number of Bitcoins, 
-, that they would like to buy. If that argument cannot be converted to a float, the program should exit via sys.exit with an error message.
-Queries the API for the CoinDesk Bitcoin Price Index at https://api.coindesk.com/v1/bpi/currentprice.json, which returns a JSON object, among whose nested keys is the current price of Bitcoin as a float. Be sure to catch any exceptions, as with code like:
+import sys
 import requests
 
-try:
-    ...
-except requests.RequestException:
-    ...
-Outputs the current cost of 
- Bitcoins in USD to four decimal places, using , as a thousands separator.
-Hints
-Recall that the sys module comes with argv, per docs.python.org/3/library/sys.html#sys.argv.
-Note that the requests module comes with quite a few methods, per requests.readthedocs.io/en/latest, among which are get, per requests.readthedocs.io/en/latest/user/quickstart/#make-a-request, and json, per requests.readthedocs.io/en/latest/user/quickstart/#json-response-content. You can install it with:
-pip install requests
-Note that CoinDesk’s API returns a JSON response like:
-{
-   "time":{
-      "updated":"May 2, 2022 15:27:00 UTC",
-      "updatedISO":"2022-05-02T15:27:00+00:00",
-      "updateduk":"May 2, 2022 at 16:27 BST"
-   },
-   "disclaimer":"This data was produced from the CoinDesk Bitcoin Price Index (USD). Non-USD currency data converted using hourly conversion rate from openexchangerates.org",
-   "chartName":"Bitcoin",
-   "bpi":{
-      "USD":{
-         "code":"USD",
-         "symbol":"&#36;",
-         "rate":"38,761.0833",
-         "description":"United States Dollar",
-         "rate_float":38761.0833
-      },
-      "GBP":{
-         "code":"GBP",
-         "symbol":"&pound;",
-         "rate":"30,827.6198",
-         "description":"British Pound Sterling",
-         "rate_float":30827.6198
-      },
-      "EUR":{
-         "code":"EUR",
-         "symbol":"&euro;",
-         "rate":"36,800.2764",
-         "description":"Euro",
-         "rate_float":36800.2764
-      }
-   }
-}
+def get_bitcoin_price():
+    """Returns the current price of Bitcoin in USD."""
+    url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+    
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        
+        data = response.json()
+        return data["bpi"]["USD"]["rate_float"]
+    
+    except requests.RequestException:
+        sys.exit("Error: Unable to fetch Bitcoin price from CoinDesk API.")
 
+def main():
+    # Ensure correct number of command-line arguments
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python bitcoin.py [number_of_bitcoins]")
+    
+    # Try to convert the argument to a float
+    try:
+        number_of_bitcoins = float(sys.argv[1])
+    except ValueError:
+        sys.exit("Error: Please provide a valid number of Bitcoins.")
+    
+    # Get the current price of Bitcoin
+    bitcoin_price = get_bitcoin_price()
+    
+    # Calculate the cost for the specified number of Bitcoins
+    total_cost = number_of_bitcoins * bitcoin_price
+    
+    # Print the cost formatted to 4 decimal places and with thousands separator
+    print(f"The cost of {number_of_bitcoins} Bitcoins is: ${total_cost:,.4f}")
 
-'''
+if __name__ == "__main__":
+    main()
