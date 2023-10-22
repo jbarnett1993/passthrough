bdda455d64e94bf979ba3a8d222dcbcb5de1defe
jbarnett1993 <jamesbarnett1993@icloud.com>
2023-10-22 17:36:49 +0100

    adding

--- cs50.py
+++ cs50.py
@@ -2,16 +2,17 @@ import sys
 import random
 import pyfiglet
 
+print("testing")
 
 
 def main():
-    if len(sys.argv) not in [0,2]:
+    if len(sys.argv) not in [1,3]:
         sys.exit("incorrect usage")
 
-    if len(sys.argv) == 2:
-        if sys.argv[0] not in ['-f','--font']:
+    if len(sys.argv) == 3:
+        if sys.argv[1] not in ['-f','--font']:
             sys.exit("first input not correct")
-
+        print(sys.argv[2])
         font = sys.argv[2]
         if font not in pyfiglet.FigletFont.getFonts():
             sys.exit(f"Font {font} is not supported")
@@ -24,5 +25,5 @@ def main():
         result = pyfiglet.figlet_format(text, font=font)
         print(result)
 
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
