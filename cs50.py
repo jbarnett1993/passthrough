2aa79d6c37c81f39b25dbae64356ca86173f3c1a
jbarnett1993 <jamesbarnett1993@icloud.com>
2023-10-22 17:46:29 +0100

    not working

--- cs50.py
+++ cs50.py
@@ -2,17 +2,16 @@ import sys
 import random
 import pyfiglet
 
-print("testing")
-
+test = pyfiglet.figlet_format("cs50", font = "slant" ) 
+# print(test)
 
 def main():
     if len(sys.argv) not in [1,3]:
         sys.exit("incorrect usage")
-
+    
     if len(sys.argv) == 3:
         if sys.argv[1] not in ['-f','--font']:
             sys.exit("first input not correct")
-        print(sys.argv[2])
         font = sys.argv[2]
         if font not in pyfiglet.FigletFont.getFonts():
             sys.exit(f"Font {font} is not supported")
@@ -20,7 +19,8 @@ def main():
         else:
             font = random.choice(pyfiglet.FigletFont.getFonts())
 
-        text = input("Enter text:")
+        text = "cs50"
+        # text = input("Enter text:")
 
         result = pyfiglet.figlet_format(text, font=font)
         print(result)
