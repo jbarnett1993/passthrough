49dd0a41ac731564c70706656c3503be2c36f68d
jbarnett1993 <jamesbarnett1993@icloud.com>
2023-10-23 11:20:26 +0100

    new q

--- cs50.py
+++ cs50.py
@@ -1,39 +1,41 @@
-import sys
-import requests
 
-def get_bitcoin_price():
-    """Returns the current price of Bitcoin in USD."""
-    url = "https://api.coindesk.com/v1/bpi/currentprice.json"
-    
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        
-        data = response.json()
-        return data["bpi"]["USD"]["rate_float"]
-    
-    except requests.RequestException:
-        sys.exit("Error: Unable to fetch Bitcoin price from CoinDesk API.")
+
+menu = {
+    "Baja Taco": 4.00,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+def get_order():
+    """Prompt the user for items until they input Ctrl-D or Ctrl-Z."""
+    while True:
+        try:
+            item = input("Enter an item from the menu: ")
+            if not item:  # exit loop if name is empty
+                break
+            # names.append(item)
+        except EOFError:
+            break
+
+        cost = menu[item]
+
+    return cost
+
 
 def main():
-    # Ensure correct number of command-line arguments
-    if len(sys.argv) != 2:
-        sys.exit("Usage: python bitcoin.py [number_of_bitcoins]")
-    
-    # Try to convert the argument to a float
-    try:
-        number_of_bitcoins = float(sys.argv[1])
-    except ValueError:
-        sys.exit("Error: Please provide a valid number of Bitcoins.")
-    
-    # Get the current price of Bitcoin
-    bitcoin_price = get_bitcoin_price()
-    
-    # Calculate the cost for the specified number of Bitcoins
-    total_cost = number_of_bitcoins * bitcoin_price
-    
-    # Print the cost formatted to 4 decimal places and with thousands separator
-    print(f"The cost of {number_of_bitcoins} Bitcoins is: ${total_cost:,.4f}")
+    items = get_order()
+
+    if not items:
+        sys.exit("No names were provided.")
+
+    output = "f{cost}"
+    print(output)
 
 if __name__ == "__main__":
     main()
