cbc84016d2e465de136ebae42a5996dabd003719
jbarnett1993 <64900909+jbarnett1993@users.noreply.github.com>
2023-11-06 19:24:33 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,29 +4,39 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 import numpy as np
-from datetime import datetime, timedelta
+from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 mgr = dm.BbgDataManager()
 
-
+# Dictionary of curves
 curves = {
     'AUD': '1', 'CAD': '7', 'CHF': '82',
     'GER': '16', 'GBP': '22', 'JGB': '18',
     'nzd': '49', 'sek': '21', 'BTP': '40'
 }
 
+# Define start and end dates for the last week and last month
 start_date = (datetime.today() - relativedelta(days=7)).strftime('%Y-%m-%d')
 end_date = datetime.today().strftime('%Y-%m-%d')
+start_last_month = (datetime.today() - relativedelta(months=1)).strftime('%Y-%m-%d')
+end_last_month = start_last_month
 
+# Initialize lists for rolldowns
 rolldowns = []
+last_week_values = []
+last_month_values = []
+
+# Loop through each curve to fetch data
 for ccy, curve in curves.items():
-    resp = LocalTerminal.get_reference_data('YCGT'+curve.zfill(4)+' Index','CURVE_TENOR_RATES')
-    df = resp.as_frame()['CURVE_TENOR_RATES']['YCGT'+curve.zfill(4)+' Index']
+    curve_id = 'YCGT' + curve.zfill(4) + ' Index'
+    resp = LocalTerminal.get_reference_data(curve_id, 'CURVE_TENOR_RATES')
+    df = resp.as_frame()['CURVE_TENOR_RATES'][curve_id]
     tenors = df['Tenor']
     tenor_tickers = df['Tenor Ticker']
     mid_yield = df['Mid Yield']
 
+    # Calculate the rolldown
     rolldown = np.diff(mid_yield).round(4)
     rolldown = np.insert(rolldown, 0, np.nan)  # Set first rolldown as NaN
 
@@ -38,29 +48,43 @@ for ccy, curve in curves.items():
         'yield': mid_yield,
         'Rolldown': rolldown
     })
-
     rolldowns.append(df_info)
 
 # Concatenate rolldown DataFrames into a single DataFrame
 rolldowns = pd.concat(rolldowns, ignore_index=True)
 rolldowns.to_csv("all_curves.csv")
+
+# Fetch historical data
 sids = mgr[rolldowns['Tenor Ticker'].to_list()]
-
-
-last_week = sids.get_historical('YLD_YTM_MID', start_date, start_date).T
-
-# last_week['Tenor Ticker'] = last_week.index
+last_week = sids.get_historical('YLD_YTM_MID', start_date, end_date).T
 last_week.reset_index(inplace=True)
-last_week.columns = ['Tenor Ticker','last_week_yield']
+last_week.columns = ['Tenor Ticker', 'last_week_yield']
 
+last_month = sids.get_historical('YLD_YTM_MID', start_last_month, end_last_month).T
+last_month.reset_index(inplace=True)
+last_month.columns = ['Tenor Ticker', 'last_month_yield']
+
+# Merge the historical data with the rolldown DataFrame
 combined_rolldowns = rolldowns.merge(last_week, on="Tenor Ticker")
-combined_rolldowns['ccy_test'] = combined_rolldowns['Currency'].eq(combined_rolldowns['Currency'].shift())
-combined_rolldowns['last_week_rolldown'] = np.where(combined_rolldowns['ccy_test'] == True, combined_rolldowns['last_week_yield'].diff(),np.nan)
+combined_rolldowns = combined_rolldowns.merge(last_month, on="Tenor Ticker")
 
+# Add columns for last week and last month rolldown
+combined_rolldowns['ccy_test'] = combined_rolldowns['Currency'].eq(combined_rolldowns['Currency'].shift())
+
+combined_rolldowns['last_week_rolldown'] = np.where(
+    combined_rolldowns['ccy_test'],
+    combined_rolldowns['yield'] - combined_rolldowns.groupby('Currency')['last_week_yield'].shift(),
+    np.nan
+)
+
+combined_rolldowns['last_month_rolldown'] = np.where(
+    combined_rolldowns['ccy_test'],
+    combined_rolldowns['yield'] - combined_rolldowns.groupby('Currency')['last_month_yield'].shift(),
+    np.nan
+)
+
+# Save the combined DataFrame to CSV
 combined_rolldowns.to_csv('combined_rolldowns.csv')
 
-
-
+# Print the combined DataFrame
 print(combined_rolldowns)
-
-
