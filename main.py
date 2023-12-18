ac15a3199f60ff8397e7eb3bfac9961e9f276070
jbarnett1993 <64900909+jbarnett1993@users.noreply.github.com>
2023-12-18 14:14:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 import pandas as pd
-import numpy as np
-import tia.bbg.datamgr as dm
-import tia.analysis.ta as ta
-import tia.analysis.model as model
-from matplotlib import gridspec
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_pdf import PdfPages
+import tia.bbg.datamgr as dm
 
 mgr = dm.BbgDataManager()
 
+# Define the date range for the past year
 start_date = (datetime.today() - relativedelta(years=1)).strftime('%Y-%m-%d')
 end_date = datetime.today().strftime('%Y-%m-%d')
 
-sids = ["EURUSD", "USDNOK", "GBPUSD","USDCAD","USDJPY","USDSEK","AUDUSD", "USDCHF","NZDUSD"]
-vols = {} 
+# List of currency pairs
+sids = ["EURUSD", "USDNOK", "GBPUSD", "USDCAD", "USDJPY", "USDSEK", "AUDUSD", "USDCHF", "NZDUSD"]
+volatility_data = {}
+
 for sid in sids:
+    # Construct the Bloomberg tickers for 3-month and 1-year volatility
     q_ticker = sid + "V3M Curncy"
     y_ticker = sid + "V1Y Curncy"
+    
+    # Fetch the historical data for 3-month and 1-year volatility
+    try:
+        df_3m = mgr[q_ticker].get_historical('PX_LAST', start_date, end_date)
+        df_1y = mgr[y_ticker].get_historical('PX_LAST', start_date, end_date)
+        
+        # Join the two DataFrames on their index (date)
+        df_combined = df_3m.join(df_1y, lsuffix='_3m', rsuffix='_1y')
+        
+        # Store the combined dataframe in the dictionary with key as SID
+        volatility_data[sid] = df_combined
+    except Exception as e:
+        print(f"An error occurred while fetching data for {sid}: {e}")
 
-    df = mgr[q_ticker].get_historical('PX_LAST', start_date, end_date)
-    vols[sid] = df
+# Now volatility_data contains the combined dataframes for each SID with both 3-month and 1-year volatilities
