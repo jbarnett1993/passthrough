bc6d06d6cde4a3c233d949144651187dec0e971f
jbarnett1993 <64900909+jbarnett1993@users.noreply.github.com>
2023-12-18 14:19:21 +0000

    Update main.py

--- main.py
+++ main.py
@@ -32,3 +32,44 @@ for sid in sids:
         print(f"An error occurred while fetching data for {sid}: {e}")
 
 # Now volatility_data contains the combined dataframes for each SID with both 3-month and 1-year volatilities
+
+# Number of variables we're plotting.
+num_vars = len(volatility_data)
+
+# Create a figure for the radar chart.
+fig, ax = plt.subplots(figsize=(6, 6), subplot_kw=dict(polar=True))
+
+# Compute angle each bar is centered on:
+angles = np.linspace(0, 2 * np.pi, num_vars, endpoint=False).tolist()
+
+# The plot is circular, so we need to "complete the loop" and append the start to the end.
+angles += angles[:1]
+
+# Draw one axe per variable and add labels
+ax.set_theta_offset(np.pi / 2)
+ax.set_theta_direction(-1)
+
+# Draw the outline of our data.
+ax.set_thetagrids(np.degrees(angles[:-1]), volatility_data.keys())
+
+# Prepare the percentile data for each currency pair
+for sid, df in volatility_data.items():
+    # Calculate the percentile rank of the 3m and 1y volatilities.
+    df['percentile_3m'] = df[df.columns[0]].rank(pct=True) * 100
+    df['percentile_1y'] = df[df.columns[1]].rank(pct=True) * 100
+    
+    # Fetch the last percentile ranks for the plot
+    values = df[['percentile_3m', 'percentile_1y']].iloc[-1].tolist()
+    # Complete the loop
+    values += values[:1]
+    
+    # Plot data and fill with color
+    ax.plot(angles, values, linewidth=1, linestyle='solid', label=sid)
+    ax.fill(angles, values, alpha=0.25)
+
+# Add a legend and title for the chart
+ax.legend(loc='upper right', bbox_to_anchor=(0.1, 0.1))
+plt.title('FX Volatility Percentile Ranks')
+
+# Show the plot
+plt.show()
