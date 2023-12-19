8f66dd22f53e7bc4c37aacec2697b3e86ec00fa2
jbarnett1993 <jamesbarnett1993@icloud.com>
2023-12-19 07:20:57 +0000

    adding table for percentiles

--- main.py
+++ main.py
@@ -15,8 +15,7 @@ end_date = datetime.today().strftime('%Y-%m-%d')
 
 # List of currency pairs
 sids = ["EURUSD", "USDNOK", "GBPUSD", "USDCAD", "USDJPY", "USDSEK", "AUDUSD", "USDCHF", "NZDUSD"]
-volatility_data = {}
-
+percentiles = {'percentile_3m': {}, 'percentile_1y': {}}
 for sid in sids:
     # Construct the Bloomberg tickers for 3-month and 1-year volatility
     q_ticker = sid + "V3M Curncy"
@@ -35,15 +34,28 @@ for sid in sids:
         last_1y_vol = df_combined.iloc[-1]['PX_LAST_1y']
         percentile_3m = percentileofscore(df_combined['PX_LAST_3m'], last_3m_vol)
         percentile_1y = percentileofscore(df_combined['PX_LAST_1y'], last_1y_vol)
-
+        percentiles['percentile_3m'][sid] = percentile_3m
+        percentiles['percentile_1y'][sid] = percentile_1y
 
 
     except Exception as e:
         print(f"An error occurred while fetching data for {sid}: {e}")
+percentile_df = pd.DataFrame(percentiles)
+print(percentile_df)
+
+'''
+        percentile_3m  percentile_1y
+EURUSD       7.442748      17.175573
+USDNOK       8.396947      14.122137
+GBPUSD       1.526718       6.106870
+USDCAD       3.435115       1.908397
+USDJPY      42.175573      45.419847
+USDSEK      21.755725      27.862595
+AUDUSD       6.106870      11.259542
+USDCHF       5.725191      11.259542
+NZDUSD      12.595420      12.977099
 
 
 
-# # Output the results
 
-# for sid, data in volatility_data.items():
-#     print(f"{sid} - 3M Volatility Percentile: {data['percentile_3m']}%, 1Y Volatility Percentile: {data['percentile_1y']}%")
+'''
