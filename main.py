67f8edc5fb92695337cab66221adbc8ee9314ae3
jbarnett1993 <jamesbarnett1993@icloud.com>
2023-12-27 10:41:11 +0000

    adding the table id like to move to a graph

--- main.py
+++ main.py
@@ -1,44 +1,58 @@
-import pandas as pd
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
-import tia.bbg.datamgr as dm
-import numpy as np
-from math import pi
-import matplotlib.pyplot as plt
-from scipy.stats import percentileofscore
+# import pandas as pd
+# from datetime import datetime
+# from dateutil.relativedelta import relativedelta
+# import tia.bbg.datamgr as dm
+# import numpy as np
+# from math import pi
+# import matplotlib.pyplot as plt
+# from scipy.stats import percentileofscore
 
-mgr = dm.BbgDataManager()
+# mgr = dm.BbgDataManager()
 
-# Define the date range for the past year
-start_date = (datetime.today() - relativedelta(years=3)).strftime('%Y-%m-%d')
-end_date = datetime.today().strftime('%Y-%m-%d')
+# # Define the date range for the past year
+# start_date = (datetime.today() - relativedelta(years=3)).strftime('%Y-%m-%d')
+# end_date = datetime.today().strftime('%Y-%m-%d')
 
-# List of currency pairs
-sids = ["EURUSD", "USDNOK", "GBPUSD", "USDCAD", "USDJPY", "USDSEK", "AUDUSD", "USDCHF", "NZDUSD"]
+# # List of currency pairs
+# sids = ["EURUSD Curncy", "USDNOK Curncy", "GBPUSD Curncy", "USDCAD Curncy", "USDJPY Curncy", "USDSEK Curncy", "AUDUSD Curncy", "USDCHF Curncy", "NZDUSD Curncy"]
 
-f_sids ={"EURUSD":"EUR12M Curncy", "USDNOK": "NOK12M Curncy", "GBPUSD": "GBP12M Curncy", "USDCAD": "CAD12M Curncy", "USDJPY": "JPY12M Curncy",
-       "USDSEK": "SEK12M Curncy", "AUDUSD":"AUD12M Curncy", "USDCHF": "CHF12M Curncy", "NZDUSD":"NZD12M Curncy" } 
-
-
-fwd_prices = [] 
-for ccy, fwd in f_sids.items():
-    fwd = mgr[fwd].PX_LAST
-    fwd_prices.append(fwd)
+# f_sids ={"EURUSD":"EUR12M Curncy", "USDNOK": "NOK12M Curncy", "GBPUSD": "GBP12M Curncy", "USDCAD": "CAD12M Curncy", "USDJPY": "JPY12M Curncy",
+#        "USDSEK": "SEK12M Curncy", "AUDUSD":"AUD12M Curncy", "USDCHF": "CHF12M Curncy", "NZDUSD":"NZD12M Curncy" } 
 
 
+# fwd_prices = pd.DataFrame()
+# for ccy, fwd in f_sids.items():
+#     spot = mgr[ccy +' Curncy'].PX_LAST
+#     fwd = mgr[fwd].PX_LAST
+#     fwd_prices.at['spot',ccy] = spot
+#     fwd_prices.at['fwd',ccy] = fwd
+#     fwd_prices.at['implied vol', ccy] = mgr[ccy + 'V3M Curncy'].PX_LAST
 
-print(fwd_prices)
+# fwd_prices = fwd_prices.T
+
+# fwd_prices['fx_fwd_implied_carry'] = ((fwd_prices['fwd'] - fwd_prices['spot']) / fwd_prices["spot"]) * 100 *-1
+
+# fwd_prices.to_csv("df.csv")
+# print(fwd_prices)
+# # df = pd.read_csv("df.csv")
+# # print(df.index.values())
+
+
 '''
-            spot         fwd  implied vol     carry
-EURUSD    1.0977    1.115750        6.305 -1.644347
-USDNOK   10.2666   10.196700       11.695  0.680849
-GBPUSD    1.2754    1.276243        6.920 -0.066097
-USDCAD    1.3335    1.327602        5.500  0.442295
-USDJPY  143.6800  136.355000        9.645  5.098135
-USDSEK   10.1588   10.009900       10.715  1.465724
-AUDUSD    0.6767    0.681119        9.360 -0.653022
-USDCHF    0.8605    0.829733        6.545  3.575479
-NZDUSD    0.6272    0.625943        9.555  0.200415
+            spot         fwd  implied vol  fx_fwd_implied_carry
+ccy
+EURUSD    1.1054    1.123550        6.610             -1.641940
+USDNOK   10.1669   10.105200       12.105              0.606871
+GBPUSD    1.2716    1.273250        7.300             -0.129758
+USDCAD    1.3200    1.314959        5.730              0.381894
+USDJPY  142.5800  135.438900       10.315              5.008486
+USDSEK    9.9881    9.849184       11.095              1.390815
+AUDUSD    0.6832    0.687340        9.610             -0.605972
+USDCHF    0.8528    0.823542        7.100              3.430816
+NZDUSD    0.6323    0.631087        9.705              0.191839
+
+
+
 
 
 '''
