767c24e63d54dfd551cd34e8a1cb13eb9bccf977
jbarnett1993 <jamesbarnett1993@icloud.com>
2023-12-28 14:52:19 +0000

    new problem

--- main.py
+++ main.py
@@ -13,30 +13,68 @@ from tia.bbg import LocalTerminal
 mgr = dm.BbgDataManager()
 
 start_date = (datetime.today() - relativedelta(years=1)).strftime('%Y-%m-%d')
-end_date = datetime.today().strftime('%Y-%m-%d')
-# end_date = datetime.strptime('2023-12-15','%Y-%m-%d')
+# end_date = datetime.today().strftime('%Y-%m-%d')
+end_date = datetime.strptime('2023-12-15','%Y-%m-%d')
 
-sid = ["EURUSD"]
-dates = ["1M","2M","3M","4M","5M","6M","7M","8M", "9M","1Y"]
+sids = ["TWI USSP Index",
+"TWI NKSP Index",
+"TWI JPSP Index",
+"TWI SFSP Index",
+"TWI EUSP Index",
+"TWI BPSP Index",
+"TWI ADSP Index",
+"TWI SKSP Index",
+"TWI CDSP Index",
+"TWI NDSP Index",]
 
-sids = []
-for date in dates:
-    ticker = sid[0] + "V" + date + " Curncy" 
-    sids.append(ticker)
+# create an empty dataframe to store the results
+results = pd.DataFrame([])
+
+# iterate over the list of securities
+for sid in sids:
+    df = mgr.get_historical(sid, ['PX_LAST'], start_date, end_date)
+    df[sid+'_100dma'] = df['PX_LAST'].rolling(window=100).mean()
+    df[sid+'_RSI']= ta.RSI(df['PX_LAST'],n=30)
+
+    #calculate divergence from 100 day MA 
+
+    df[sid+'_divergence'] = ((df['PX_LAST'] - df[sid+'_100dma'])/df['PX_LAST']) * 100
+
+    results = pd.concat([results, df], axis=1)
+
+results = results.last('7D')
+
+# Calculate average rank of deviation and RSI 
+deviation_columns = results.filter(like='_divergence').columns 
+
+RSI_columns = results.filter(like='_RSI').columns
+ranked_deviations = results[deviation_columns].rank("columns", ascending=False)
+
+ranked_rsi = results[RSI_columns].rank("columns", ascending=False)
 
 
-df = mgr.get_historical(sids, ['PX_LAST'], start_date, end_date)
-percentiles = df.quantile([0.1, 0.25, 0.5,0.75,0.9])
-print(percentiles)
-current_vol = df.iloc[-1]
-plt.figure(figsize=(10,7))
-plt.plot(dates,current_vol,label="current vol",marker="o")
-plt.fill_between(dates,percentiles.iloc[0],percentiles.iloc[4],color="grey",alpha=0.1,label="10/90 percentile")
-plt.fill_between(dates,percentiles.iloc[1],percentiles.iloc[3],color="grey",alpha=0.3,label="25/75 percentile")
-plt.plot(dates,percentiles.iloc[2],label="50th percentile",linestyle="--",color="red")
-plt.title("EURUSD Vol Term Structure vs historical level (1y lookback)")
-plt.xlabel("Tenor")
-plt.ylabel("Volatility")
-plt.legend()
-plt.grid(True)
-plt.show()
+deviation_mean = ranked_deviations.mean().to_frame().T
+rsi_mean = ranked_rsi.mean().to_frame().T
+
+momentum = pd.DataFrame([])
+
+for sid in sids:
+    col = rsi_mean.filter(like=sid).columns 
+    col2 = deviation_mean.filter(like=sid).columns
+    mom = rsi_mean[col].values + deviation_mean[col2].values
+    momentum[sid] = pd.DataFrame(mom)   
+    mmomentum = pd.concat([momentum, momentum[sid]])
+
+ranked_momentum = momentum.rank("columns", ascending=True)
+
+ranked_momentum = ranked_momentum.T
+ranked_momentum.reset_index(inplace=True)
+ids = ranked_momentum["index"].to_list()
+resp = LocalTerminal.get_reference_data(ids,["SECURITY_NAME"],ignore_field_error=1)
+names = resp.as_frame()
+ranked_momentum.columns = ["index", "score"]
+ranked_momentum.set_index("index", inplace=True)
+ranked_momentum = ranked_momentum.merge(names, how='left', left_index=True, right_index=True)
+
+
+print(ranked_momentum)
