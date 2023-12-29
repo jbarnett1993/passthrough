e7ebd53f9a639e5bf68a233fd3866b084e4431f7
jbarnett1993 <64900909+jbarnett1993@users.noreply.github.com>
2023-12-29 11:51:02 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,49 @@
 import fitz  # PyMuPDF
 
-def create_toc_page(doc, titles_and_pages):
+def create_toc_content(titles_and_pages):
     """
-    Create a TOC page with given titles and their starting page numbers.
+    Create a TOC content as a single page PDF.
     """
-    toc_page = doc.new_page(width=612, height=792)  # Create a new page for TOC
+    # Create a new PDF for TOC content
+    toc_doc = fitz.open()
+    toc_page = toc_doc.new_page(width=612, height=792)  # Standard letter size
+
+    # Add TOC title
     toc_page.insert_text((72, 72), "Table of Contents", fontsize=16, fontname="helv")
 
+    # Add TOC entries
     y_position = 100
     for title, page in titles_and_pages:
         toc_text = f"{title} - Start on Page {page}"
         toc_page.insert_text((72, y_position), toc_text, fontsize=12, fontname="helv")
         y_position += 20
 
+    return toc_doc
+
 def merge_pdfs(paths, output):
-    doc = fitz.open()  # Create a new PDF
+    merged_doc = fitz.open()  # Create a new PDF for the merged document
 
     # List to hold titles and their starting page numbers
     titles_and_pages = []
-    current_page = 1  # Start from 1 to account for the TOC page
+    current_page = 2  # Start from 2 as the first page will be the TOC
 
     for path in paths:
         pdf_doc = fitz.open(path)
         title = path.rsplit('/', 1)[-1].replace('.pdf', '')
         titles_and_pages.append((title, current_page))
 
-        doc.insert_pdf(pdf_doc)
+        merged_doc.insert_pdf(pdf_doc)
         current_page += len(pdf_doc)
 
-    # Create TOC page
-    create_toc_page(doc, titles_and_pages)
+    # Create TOC content
+    toc_doc = create_toc_content(titles_and_pages)
+
+    # Insert TOC as the first page
+    merged_doc.insert_pdf(toc_doc, to_page=0)
 
     # Save the merged PDF
-    doc.save(output)
-    doc.close()
+    merged_doc.save(output)
+    merged_doc.close()
 
 # Example usage
 paths = ['pdf1.pdf', 'pdf2.pdf', 'pdf3.pdf']  # Replace with your actual file names
