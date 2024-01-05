a30e49da228c0f73ca1c533a44e3f8f7740e7f40
jbarnett1993 <jamesbarnett1993@icloud.com>
2024-01-05 08:22:12 +0000

    loop dictionary

--- main.py
+++ main.py
@@ -1,83 +1,18 @@
-''''
-ds = Dataset('GSDEER_GSFEER')
-gs_data = {} 
-prices = {}
-master_dict = {}
-for ccy, asset_id in dict.items():
-    # print(ccy,asset_id)
-    resp = ds.get_data(datetime.date.today(), assetId=asset_id, limit=500)
-    if ccy in ['AUDUSD','EURUSD','NZDUSD','GBPUSD']:
-        resp['gsdeer'] = 1/resp['gsdeer']
-    else:
-        continue
-    cols = ['year','quarter']    
-    resp['currency'] = ccy + ' Curncy'
-    resp['period'] =  resp[cols].astype(str).apply('-'.join, axis=1)
-    resp['period'] = pd.to_datetime(resp['period'])
-    resp = resp[['currency','period','gsdeer']]
-    resp.set_index('period', inplace=True)
-    resp = resp[resp.index.year >= 2000]
-    gs_data[ccy] = resp
-    
-
-    bbgresp = pd.DataFrame()
-
-
-    start_date = datetime.date(2000, 1, 1) 
-    end_date = datetime.date.today()
-    bbgresp['price'] = mgr[ccy + ' Curncy'].get_historical('PX_LAST', start_date, end_date)
-    bbgresp['currency'] = ccy + ' Curncy' 
-    bbgresp = bbgresp[['currency','price']]
-    prices[ccy] = bbgresp
-    
-    df = bbgresp.merge(resp,how='right',on='currency')
-    master_dict[ccy] = df
-
-print(master_dict['EURUSD'])
-
-             currency   price    gsdeer
-0       EURUSD Curncy  1.0200  1.190476
-1       EURUSD Curncy  1.0306  1.190476
-2       EURUSD Curncy  1.0344  1.190476
-3       EURUSD Curncy  1.0286  1.190476
-4       EURUSD Curncy  1.0295  1.190476
-...               ...     ...       ...
-626295  EURUSD Curncy  1.1074  1.219512
-626296  EURUSD Curncy  1.1039  1.219512
-626297  EURUSD Curncy  1.0951  1.219512
-626298  EURUSD Curncy  1.0907  1.219512
-626299  EURUSD Curncy  1.0943  1.219512
-
-
-
-                 currency    gsdeer
-period
-2000-01-01  EURUSD Curncy  1.190476
-2000-04-01  EURUSD Curncy  1.176471
-2000-07-01  EURUSD Curncy  1.190476
-2000-10-01  EURUSD Curncy  1.176471
-2001-01-01  EURUSD Curncy  1.204819
-...                   ...       ...
-2023-10-01  EURUSD Curncy  1.219512
-2024-01-01  EURUSD Curncy  1.234568
-2024-04-01  EURUSD Curncy  1.219512
-2024-07-01  EURUSD Curncy  1.219512
-2024-10-01  EURUSD Curncy  1.219512
-
-[100 rows x 2 columns]
-                 currency   price
+'''
+                 currency   price    gsdeer
 date
-2000-01-03  EURUSD Curncy  1.0200
-2000-01-04  EURUSD Curncy  1.0306
-2000-01-05  EURUSD Curncy  1.0344
-2000-01-06  EURUSD Curncy  1.0286
-2000-01-07  EURUSD Curncy  1.0295
-...                   ...     ...
-2023-12-28  EURUSD Curncy  1.1074
-2023-12-29  EURUSD Curncy  1.1039
-2024-01-02  EURUSD Curncy  1.0951
-2024-01-03  EURUSD Curncy  1.0907
-2024-01-04  EURUSD Curncy  1.0942
+2000-01-03  EURUSD Curncy  1.0200  1.190476
+2000-01-04  EURUSD Curncy  1.0306  1.190476
+2000-01-05  EURUSD Curncy  1.0344  1.190476
+2000-01-06  EURUSD Curncy  1.0286  1.190476
+2000-01-07  EURUSD Curncy  1.0295  1.190476
+...                   ...     ...       ...
+2023-12-29  EURUSD Curncy  1.1039  1.219512
+2024-01-02  EURUSD Curncy  1.0951  1.234568
+2024-01-03  EURUSD Curncy  1.0907  1.234568
+2024-01-04  EURUSD Curncy  1.0950  1.234568
+2024-01-05  EURUSD Curncy  1.0923  1.234568
+
 
 
 '''
