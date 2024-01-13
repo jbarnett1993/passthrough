5cd43efb281fed294ff94b7db5ad8081b2aa83be
jbarnett1993 <jamesbarnett1993@icloud.com>
2024-01-13 19:14:43 +0000

    moving to an fxo pricer problem

--- main.py
+++ main.py
@@ -1,72 +1,34 @@
-import QuantLib as ql
+from __future__ import division
 import pandas as pd
 import tia.bbg.datamgr as dm
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
-from tia.bbg import LocalTerminal
+import numpy as np
+from scipy.stats import norm
+import math
 
-# # will use this later when i want to use bbg data more 
-# resp = LocalTerminal.get_reference_data('eurusd curncy', 'dflt_vol_surf_bid')
-
-# resp = resp.as_frame()
-# print(resp.iloc[0]['dflt_vol_surf_bid'])
+currency_pair = "EURUSD"
 
 mgr = dm.BbgDataManager()
 
-currency_pair = "EURUSD"
-Spot = mgr[currency_pair + " Curncy"].PX_LAST
-Strike = 1.0991
-Sigma = 0.06397
-Ccy1Rate = 0.03832
-Ccy2Rate = 0.05301
-OptionType = ql.Option.Call
 
-# Option dates in quantlib objects
-EvaluationDate = ql.Date(13, 1,2024)
-SettlementDate = ql.Date(17, 4, 2024) #Evaluation +2
-ExpiryDate = ql.Date(15, 4, 2024) #Evaluation + term which is 1 week
-DeliveryDate = ql.Date(19, 1, 2024) #Expiry +2
-NumberOfDaysBetween = ExpiryDate - EvaluationDate
-
-#Generate continuous interest rates
-EurRate = Ccy1Rate
-UsdRate = Ccy2Rate
-
-SpotGlobal = ql.SimpleQuote(Spot)
-SpotHandle = ql.QuoteHandle(SpotGlobal)
-VolGlobal = ql.SimpleQuote(Sigma)
-VolHandle = ql.QuoteHandle(VolGlobal)
-UsdRateGlobal = ql.SimpleQuote(UsdRate)
-UsdRateHandle = ql.QuoteHandle(UsdRateGlobal)
-EurRateGlobal = ql.SimpleQuote(EurRate)
-EurRateHandle = ql.QuoteHandle(EurRateGlobal)
-
-#Settings such as calendar, evaluationdate; daycount
-Calendar = ql.UnitedStates(ql.UnitedStates.Settlement)
-ql.Settings.instance().evaluationDate = EvaluationDate
-DayCountRate = ql.Actual360()
-DayCountVolatility = ql.ActualActual(ql.ActualActual.ISDA)
-
-#Create rate curves, vol surface and GK process
-RiskFreeRateEUR = ql.YieldTermStructureHandle(ql.FlatForward(0, Calendar, EurRateHandle, DayCountRate))
-RiskFreeRateUSD = ql.YieldTermStructureHandle(ql.FlatForward(0, Calendar, UsdRate, DayCountRate))
-Volatility = ql.BlackVolTermStructureHandle(ql.BlackConstantVol(0, Calendar, VolHandle, DayCountVolatility))
-GKProcess = ql.GarmanKohlagenProcess(SpotHandle, RiskFreeRateEUR, RiskFreeRateUSD, Volatility)
-
-#Generate option
-Payoff = ql.PlainVanillaPayoff(OptionType, Strike)
-Exercise = ql.EuropeanExercise(ExpiryDate)
-Option = ql.VanillaOption(Payoff, Exercise)
-Option.setPricingEngine(ql.AnalyticEuropeanEngine(GKProcess))
-BsPrice = Option.NPV()
+spot = mgr[currency_pair + " Curncy"].PX_LAST
+# spot = 1.0951
+strike = 1.0991
+ccy1 = 0.03832 # EUR
+ccy2 = 0.05301 # USD
+sigma = 0.06397
+days = 90 
+t = days/365
+f = 1.101070
 
 
 
-ql.Settings.instance().evaluationDate = EvaluationDate
-print("Premium is:", Option.NPV()*1000000/Spot)
-print("Gamma is:", Option.gamma()*1000000*Spot/100)
-print("Vega is:", Option.vega()*1000000*(1/100)/Spot)
-print("Theta is:", Option.theta()*1000000*(1/365)/Spot)
-print("Delta is:", Option.delta()*1000000)
+def fxo_pricer(F, K, t, ccy2, sigma):
+    d1 = (math.log(F/K) +  0.5*sigma**2*t ) / (sigma*math.sqrt(t))
+    d2 = d1 - sigma*math.sqrt(t)
+    c = math.exp(-ccy2*t)*(F*norm.cdf(d1) - K*norm.cdf(d2))
+    p = math.exp(-ccy2*t)*(-F*norm.cdf(-d1) + K*norm.cdf(-d2))
+    print(c, p ,d1, d2)
 
-
+fxo_pricer(f,strike,t,ccy2,sigma)
