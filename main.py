5c3b24b8c48ad23292a58be714158de0518dc9a6
jbarnett1993 <jamesbarnett1993@icloud.com>
2024-01-14 11:54:07 +0000

    datetime question

--- main.py
+++ main.py
@@ -1,34 +1,13 @@
-from __future__ import division
-import pandas as pd
-import tia.bbg.datamgr as dm
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
-import numpy as np
-from scipy.stats import norm
-import math
 
 currency_pair = "EURUSD"
-
-mgr = dm.BbgDataManager()
-
-
-spot = mgr[currency_pair + " Curncy"].PX_LAST
-# spot = 1.0951
-strike = 1.0991
-ccy1 = 0.03832 # EUR
-ccy2 = 0.05301 # USD
-sigma = 0.06397
-days = 90 
-t = days/365
-f = 1.101070
+ExpiryDate = ql.Date(18, 4, 2024)
+datetime_expiry = datetime()
+Strike = 1.0991
+Sigma = LocalTerminal.get_reference_data(currency_pair+ " Curncy","RK311",RK315=ExpiryDate) 
+Ccy1Rate = 0.03832
+Ccy2Rate = 0.05301
+OptionType = ql.Option.Call
+short_long = 1 # 1 for long, -1 for short
 
 
-
-def fxo_pricer(F, K, t, ccy2, sigma):
-    d1 = (math.log(F/K) +  0.5*sigma**2*t ) / (sigma*math.sqrt(t))
-    d2 = d1 - sigma*math.sqrt(t)
-    c = math.exp(-ccy2*t)*(F*norm.cdf(d1) - K*norm.cdf(d2))
-    p = math.exp(-ccy2*t)*(-F*norm.cdf(-d1) + K*norm.cdf(-d2))
-    print(c, p ,d1, d2)
-
-fxo_pricer(f,strike,t,ccy2,sigma)
+April 22nd, 2024
