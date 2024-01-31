1e27759e12b1fe1e5a28db12fd17b2e651f08755
jbarnett1993 <jamesbarnett1993@icloud.com>
2024-01-31 12:02:57 +0000

    quiz

--- main.py
+++ main.py
@@ -39,3 +39,22 @@ with PdfPages('bollinger_bands.pdf') as export_pdf:
             df.at[i, 'Position'] = current_position
 
         # ... (summary DataFrame and plotting code remain the same) ...
+
+'''
+Happy Quiz Day!! Enjoy and good luck.
+
+1	Lunar New Year 2024 is the year of what animal?
+2	What year did the Scottish Independence Referendum take place?
+3	In medicine, how is the 'Carpal' region of the body more commonly known?
+4	"First Lord of the Treasury” is inscribed on the brass letterbox of which door in London?
+5	Which now famous TV chef started cooking at the age of eight in his parents’ pub, ‘The Cricketers, in Clavering, Essex?
+6	If the Earth were made into a black hole, what would be the diameter of its event horizon - (i) 20mm (ii) 20cm (iii) 20m?
+7	In men’s football, who is the most capped player for (i) England (ii) Wales (iii) Scotland
+8	Throughout the Harry Potter novels, Harry is mentioned by name 18,956 times. Which character is the second most mentioned coming in at a total of 6,464?
+9	What is the name of the third-longest river in the world?
+10	Which films do the following quotes come from?
+(i) “I’m also just a girl, standing in front of a boy, asking him to love her.”
+(ii) ""Just keep swimming.”
+(iii) “Jack, I Want You To Draw Me Like One Of Your French Girls.”
+
+'''
